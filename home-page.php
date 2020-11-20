d74b9b77434cdc74836a08485434639e352aeb9d
balanejacob <balanejacob1@gmail.com>
2020-11-20 21:14:57 +0800

    added new files

---
+++ home-page.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Blood+</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<link href="style.css" rel="stylesheet">
+	
+</head>
+<body onload="displayContent(0)">
+
+<div id="side" class="side-panel bg-dark">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <div class="position-relative h-100">
+    <div class="container text-center">
+      <img src="img/sample-pic.png" alt="" class="w-50">
+      <p class="text-light p-2">Name of User</p>
+    </div>
+    <div class="side-panel-options">
+      <a href="">Something Here</a>
+      <a href="">Something Here</a>
+      <a href="">Something Here</a>
+    </div>
+  </div>
+</div>
+
+
+<div class="nav container-fluid bg-dark text-light p-2 d-flex justify-content-between">
+  <p class="openside m-0" style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776;</p>
+  <p class="center-text m-0 p-1">Blood+</p>
+  <p></p>
+</div>
+
+<div class="container-fluid bg-light text-light text-center homepage-content">
+  <div class="row">
+    <div class="col-6 p-0">
+      <button class="h-100 w-100 btn" onclick=" displayContent(0)">Find Blood</button>
+    </div>
+    <div class="col-6 p-0">
+    <button class="h-100 w-100 btn" onclick=" displayContent(1)">Donate Blood</button>
+    </div>
+  </div>
+</div>
+
+<div class="homepage-content">
+  <div class="find-blood">
+    <div class="container-fluid bg-danger p-5">
+       <p class="text-center">Find brrt brrt</p>
+    </div>
+  </div>
+
+  <div class="donate-blood text-center">
+    <div class="container-fluid p-5 d-flex align-items-center justify-content-center">
+    <p>You still need to register to become a Donor.</p>
+    </div>
+    <div class="container-fluid p-5 d-flex align-items-center justify-content-center">
+      <button class="btn btn-primary">Register Now</button>
+    </div>
+  </div>
+</div>
+
+<script>
+function openNav() {
+  document.getElementById("side").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("side").style.width = "0";
+}
+
+function displayContent(num){
+  var find = document.querySelector(".find-blood");
+  var donate = document.querySelector(".donate-blood");
+
+  find.style.display="none";
+  donate.style.display="none";
+
+  if (num==0) {
+    find.style.display="block";
+  }else{
+    donate.style.display="block";
+  }
+
+}
+
+</script>
+
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+</html>
+
+
+
---
+++ login-page.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Blood+ 	</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<link href="style.css" rel="stylesheet">
+	
+</head>
+<body>
+
+<div id="front" class="container text-center p-4 position-relative">
+    <div class="container text-center front-sub">
+        <img src="img/logo.png" alt="logo" class="front-logo">
+        <h3 class='app-name text-dark'><b>BLOOD+</b></h3>
+    </div>
+    <div class="container text-center p-4 front-sub">
+        <form action="">
+            <div class="form-group">
+                <label class="form-control text-left input-label p-0" for="">Email Address</label>
+                <input class="form-control text-center" type="text" name="" id="">
+            </div>
+            <div class="form-group">
+                <label class="form-control text-left input-label p-0" for="">Password</label>
+                <input class="form-control text-center" type="text" name="" id="">
+            </div>
+            <div class="form-group">
+                <input class="btn btn-primary px-4" type="submit" name="" value="Log In">
+            </div>
+            <p id="login-error" class="text-danger"></p>
+        </form>
+    </div>
+    <hr>
+    <div class="container-fluid padding text-center mt-5">
+        <h6>New User? | 
+            <button type="button" class="btn btn-primary text-light btn-sm">Sign up</button>
+            <button type="button" class="btn btn-outline-dark btn-sm" onclick="window.location.href='#about'">Learn More</button>
+        </h6>
+    </div>
+</div>
+<hr>
+<div class='front-info'>
+    <div id="about" class="container padding">
+        <div class="app-info text-center">
+            <div class="container">
+                <h1 class="app-what">What is Blood+?</h1>
+            </div>
+            <hr>
+            <div class="container">
+                <p class="app-what-text text-justify">
+                    Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut 
+                    aliquip ex ea commodo consequat. Duis aute 
+                    irure dolor in reprehenderit in voluptate 
+                    velit esse cillum dolore eu fugiat nulla 
+                    pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia 
+                        deserunt mollit anim id est laborum.	
+                </p>
+            </div>
+        </div>
+    </div>
+        
+    <div class="container-fluid padding text-center">
+        <div class="container">
+            <img class="app-info-icon" src="" alt="picture">
+            <h4>Find Blood Faster</h4>
+        </div>
+        <div class="container">
+            <img class="app-info-icon" src='' alt="picture">
+            <h4>Help Donate Blood</h4>
+        </div>
+    </div>
+    <hr>
+    <div class="container padding">
+        <div class="app-info text-center">
+            <div class="container">
+                <h1 class="app-what">Why Blood+?</h1>
+            </div>
+            <hr>
+            <div class="container app-what-text text-justify">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut 
+                    aliquip ex ea commodo consequat.	
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur 
+                    adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut 
+                    aliquip ex ea commodo consequat.	
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid padding text-center">
+    <hr>
+    <h5>Find Donor Now | <button type="button" class="btn btn-primary text-light">Sign up</button></h5>
+    <hr>
+</div>
+
+
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+</html>
+
+
+
