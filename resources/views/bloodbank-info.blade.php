43768aefc313cce7ff469a526515cc5d976a6a76
samkayes <samanthasubaan@gmail.com>
2021-01-25 10:20:08 +0800

    sam here

--- resources/views/bloodbank-info.blade.php
+++ resources/views/bloodbank-info.blade.php
@@ -186,14 +186,6 @@
                     <div class="container font-weight-normal">
                       <div class="row">
                         <div class="form-group col-3 py-2">
-                          <label>House No</label>
-                        </div>
-                        <div class="form-group col-9 p-0">
-                          <input type="text" class="form-control" value="{{$info->house_num}}" name="houseNo" required>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="form-group col-3 py-2">
                           <label>Street</label>
                         </div>
                         <div class="form-group col-9 p-0">
